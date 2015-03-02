edd71c58600853d5e73d06ef3331a9486f212ea9
mcelrea <mcelrea@damien-hs.edu>
2015-03-02 13:54:05 -0800

    Update to GamePlayScreen

--- core/src/com/mcelrea/GamePlayScreen.java
+++ core/src/com/mcelrea/GamePlayScreen.java
@@ -5,13 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Tech on 3/2/2015.
  */
 public class GamePlayScreen implements Screen {
+
+    World world;                        //a large collection of Bodies and Fixtures
+    OrthographicCamera camera;          //display a small portion of the world
+    Box2DDebugRenderer debugRenderer;   //display the Bodies and Fixtures
+    SpriteBatch batch;                  //used to draw Textures and Sprites
+
     @Override
     public void show() {
+        //create world without gravity
+        world = new World(new Vector2(0,0), true);
+
+        camera = new OrthographicCamera();
+        debugRenderer = new Box2DDebugRenderer();
+        batch = new SpriteBatch();
 
     }
 
@@ -19,11 +36,17 @@ public class GamePlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        world.step(1/60f, 8, 3);
+        camera.update();
+
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        camera.viewportWidth = width/25f;
+        camera.viewportHeight = height/25f;
     }
 
     @Override
